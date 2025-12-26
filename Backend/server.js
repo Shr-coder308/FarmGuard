a9d67ec7fdb60d79ed19f900c994872ce57d5d8c
Shr-coder308 <Shreyanshsoni756@gmail.com>
2025-12-26 13:53:11 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 
 mongoose.connect("mongodb://127.0.0.1:27017/farmguard").then(() => {
-  console.log("✅ MongoDB Connected");
+  console.log(" MongoDB Connected");
 });
 
 
@@ -28,12 +28,12 @@ app.post("/signup", async (req, res) => {
 
   
   const exist = await User.findOne({ email });
-  if (exist) return res.status(400).json({ message: "❌ Email already registered" });
+  if (exist) return res.status(400).json({ message: " Email already registered" });
 
   const hashed = await bcrypt.hash(password, 10);
   await User.create({ name, email, password: hashed });
 
-  res.json({ message: "✅ Signup success" });
+  res.json({ message: " Signup success" });
 });
 
 
@@ -41,10 +41,10 @@ app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  if (!user) return res.json({ message: "❌ No user found" });
+  if (!user) return res.json({ message: " No user found" });
 
   const match = await bcrypt.compare(password, user.password);
-  if (!match) return res.json({ message: "❌ Wrong password" });
+  if (!match) return res.json({ message: " Wrong password" });
 
   const token = jwt.sign({ email: user.email }, "secret123");
 
@@ -64,4 +64,4 @@ app.post("/detect", (req, res) => {
   res.json({ result: "✅ Healthy Crop Detected" });
 });
 
-app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+app.listen(5000, () => console.log(" Server running on http://localhost:5000"));
